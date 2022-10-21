b824e4e5b1fc0db8b7f8b8cff5038d71ae2017af
Mateusz <czajkamateusz999@gmail.com>
2022-10-21 22:00:27 +0200

    attempt to code square remainder with Euler criterion

--- cryptography/project_1/cryptography_tasks/tasks/square_remainder.py
+++ cryptography/project_1/cryptography_tasks/tasks/square_remainder.py
@@ -1,2 +1,15 @@
+def check_square_remainder(a, p):
+    pass
+
+
 def square_remainder():
     print("--- SQUARE REMAINDER --- ")
+
+    p = int(input("Enter p (must be prime): "))
+    group_z_multiplication = [*range(0, p)]
+
+    for num in group_z_multiplication:
+        if check_square_remainder(num, p):
+            print(f'{num} - reszta kwadratowa')
+        else:
+            print(f'{num} - nie jest resztą kwadratową')
